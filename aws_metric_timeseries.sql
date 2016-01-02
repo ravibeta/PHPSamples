0d74a3ebd86b974593769bc22a34da5fc399e86b
Ravi Rajamani <ravibeta@hotmail.com>
2016-01-01 20:42:56 -0800

    Update aws_metric_timeseries.sql

--- aws_metric_timeseries.sql
+++ aws_metric_timeseries.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Metric]
 (
-    [ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Label] NVARCHAR(50) NOT NULL DEFAULT 'UNKNOWN', 
     [Created] DATETIME NOT NULL DEFAULT getdate(), 
     [Value] REAL NOT NULL DEFAULT 0.00, 
@@ -8,13 +8,16 @@ CREATE TABLE [dbo].[Metric]
     [Units] NVARCHAR(50) NULL DEFAULT 'NA', 
     [Service] NVARCHAR(50) NULL, 
     [Region] NVARCHAR(50) NULL, 
-    [InstanceID] NVARCHAR(50) NULL, 
-    [ClusterID] NVARCHAR(50) NULL,
-    [ProjectID] NVARCHAR(50) NULL, 
-    [Created By] NVARCHAR(50) NULL
+    [InstanceID] INT NULL, 
+    [ClusterID] INT NULL, 
+    [ProjectID] INT NULL, 
+    [Created By] INT NULL
 )
 
 GO
 
 CREATE INDEX [IX_Metric_Created] ON [dbo].[Metric] ([Created])
+
 GO
+
+INSERT INTO Metric VALUES ('CPUUtilization', getdate(), 48.852, 'Gauge', 'Percent', 'EC2','us-west-2', 0,0,0,'rajamani');
