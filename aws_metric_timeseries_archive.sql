a8c145b852b308087c297b35c4b12bb17361135a
Ravi Rajamani <ravibeta@hotmail.com>
2016-01-02 15:06:35 -0800

    Update aws_metric_timeseries_archive.sql

--- aws_metric_timeseries_archive.sql
+++ aws_metric_timeseries_archive.sql
@@ -1,7 +1,7 @@
 DECLARE @ID INT;
-select @ID=ID from Metric where Created < '2014-01-01'
 while EXISTS(select * from Metric where Created < '2014-01-01')
 BEGIN
+select @ID=ID from Metric where Created < '2014-01-01'
 INSERT INTO Archive (Label, Created, Value, Type, Units, Service, Region, InstanceID, ClusterID, ProjectID, [Created By])
 SELECT Label, Created, Value, Type, Units, Service, Region, InstanceID, ClusterID, ProjectID, [Created By] from Metric WHERE ID = @ID;
 DELETE FROM Metric where Created < '2014-01-01';
